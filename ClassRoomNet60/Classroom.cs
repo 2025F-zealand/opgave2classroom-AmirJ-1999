f532bf6d209735955c4a89b87f33c58ae2ac36ee
Amir Jawad <amirjawad79@gmail.com>
2025-02-06 13:33:58 +0100

    Created new class called "Classroom" and added three properties and a default constructor.

---
+++ ClassRoomNet60/Classroom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ClassRoom
+{   // Properties
+    public string ClassName { get; set; }
+    public List<Student> StudentList { get; set; }
+    public DateTime SemesterStart { get; set; }
+
+    // Constructor
+    public ClassRoom()
+    {
+        ClassName = "Shababs klassen";
+        StudentList = new List<Student>();
+        SemesterStart = DateTime.Now; // Current date and time
+    }
+}
--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace ClassRoomNet60 { }
 public class Student
-{
+{   // Properties
     public string Name { get; private set; }
     public int BirthMonth { get; private set; }
     public int Birthday { get; private set; }
 
+    // Constructor
     public Student(string name, int birthMonth, int birthday)
     {
         Name = name;
