3691beedac6c8a31174932d397de156807cf3d60
Amir Jawad <amirjawad79@gmail.com>
2025-02-06 13:54:36 +0100

    Created a classroom instance with classname and Semesterstart datetime. Also added three students including myself. (Task 3 is stil WIP)

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        // Creating a new classroom instance
+        ClassRoom myClass = new ClassRoom
+        {
+            ClassName = "3B",
+            SemesterStart = new DateTime(2024, 2, 24) // Setting semester start date
+        };
+
+        // Adding myself and my classmates to the class
+        myClass.StudentList.Add(new Student("Amir", 5, 99));  // Myself
+        myClass.StudentList.Add(new Student("Sameet", 2, 00)); // Classmate 1
+        myClass.StudentList.Add(new Student("Hasnain", 6, 04)); // Classmate 2
+    }
+}
