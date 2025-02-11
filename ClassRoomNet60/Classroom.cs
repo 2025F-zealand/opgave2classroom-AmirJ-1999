06c59ea0d78d2b0ddc59982c9f9b2edae7ca483a
Amir Jawad <amirjawad79@gmail.com>
2025-02-11 18:17:31 +0100

    Added the method to count birthdays by season, and a test student for testing invalid month.

--- ClassRoomNet60/Classroom.cs
+++ ClassRoomNet60/Classroom.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ClassRoom
-{   // Properties
+{
+    // Properties
     public string ClassName { get; set; }
     public List<Student> StudentList { get; set; }
     public DateTime SemesterStart { get; set; }
@@ -13,5 +15,19 @@ public class ClassRoom
         ClassName = "3B";
         StudentList = new List<Student>();
         SemesterStart = DateTime.Now; // Current date and time
+    }
+
+    // Method to count students per season using LINQ
+    public void CountBirthdaysBySeason()
+    {
+        var seasonCounts = StudentList
+            .GroupBy(s => s.GetSeason()) // Grouping students by season
+            .Select(group => new { Season = group.Key, Count = group.Count() }); // Counting students per season
+
+        Console.WriteLine("Number of students born in each season:");
+        foreach (var entry in seasonCounts)
+        {
+            Console.WriteLine($"{entry.Season}: {entry.Count}");
+        }
     }
 }
--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -9,18 +9,23 @@
             SemesterStart = new DateTime(2024, 2, 24) // Setting semester start date
         };
 
-        // Adding myself and my classmates to the class
-        myClass.StudentList.Add(new Student("Amir", 5, 1999));  // Myself
-        myClass.StudentList.Add(new Student("Samet", 2, 2000)); // Classmate 1
-        myClass.StudentList.Add(new Student("Hasnain", 6, 2004)); // Classmate 2
+        // Adding students
+        myClass.StudentList.Add(new Student("Amir", 5, 1999));   // Spring
+        myClass.StudentList.Add(new Student("Samet", 2, 2000));  // Winter
+        myClass.StudentList.Add(new Student("Hasnain", 6, 2004)); // Summer
+        myClass.StudentList.Add(new Student("test", 13, 9999)); // Test invalid month
 
-        // Printing out the class name and semester start date
+        // Printing class info
         Console.WriteLine($"Class name: {myClass.ClassName}");
         Console.WriteLine($"Semester Start: {myClass.SemesterStart.ToShortDateString()}");
-        // Printing out the students in the class
+
+        // Printing students
         foreach (Student s in myClass.StudentList)
         {
             Console.WriteLine(s);
         }
+
+        // Calling the method to count birthdays by season
+        myClass.CountBirthdaysBySeason();
     }
 }
